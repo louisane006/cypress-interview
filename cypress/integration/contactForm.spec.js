ab19bbb3a4d10020449922386e56e7305c8c2e46
James Martin <jamesmartin442@gmail.com>
2021-06-14 09:13:22 +0100

    Update contactForm.spec.js
    Typo

--- cypress/integration/contactForm.spec.js
+++ cypress/integration/contactForm.spec.js
@@ -1,5 +1,5 @@
 /*
-Task: visit to https://aluminati.net, browse to the contact form, and fill out the form with whatever details you deem appropriate.
+Task: visit https://aluminati.net, browse to the contact form, and fill out the form with whatever details you deem appropriate.
 You will not be able to submit the form as it's behind a CAPTCHA, but simply filling out the data is enough for this exercise.
 */
 
